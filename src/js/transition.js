f2f643a5adcbd88d8530e845ba8f002fd175e9e6
julianaesteves <juliana.trindade@bandtec.com.br>
2022-03-13 12:31:20 -0300

    alterações - form, textos, imagens e banner/header

--- src/js/transition.js
+++ src/js/transition.js
@@ -16,3 +16,13 @@ window.addEventListener('scroll', () => {
         animeAtScroll()
     }
 })
+
+var menu = document.querySelector('nav');
+var menuIcon = document.querySelector('.menu-icon')
+var closeIcon = document.querySelector('.close-icon')
+menuIcon.addEventListener('click', ()=> {
+    menu.classList.add('active')
+})
+closeIcon.addEventListener('click', ()=> {
+    menu.classList.remove('active')
+})
