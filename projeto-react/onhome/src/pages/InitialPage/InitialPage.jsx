7b9a90a0623e24b92eebaecd75794879f5e6a171
foliveiracamara <felipe.camara@bandtec.com.br>
2022-04-02 11:35:16 -0300

    Inserindo arquitetura ReactJS ao projeto

---
+++ projeto-react/onhome/src/pages/InitialPage/InitialPage.jsx
@@ -0,0 +1,16 @@
+import "./InitialPage.css"
+
+const InitialPage = () => {
+    return (
+        <div>
+            <div className="div--mainBox">
+                
+            </div>
+            <div className="sla">
+                <div>alo</div>
+            </div>
+        </div>
+    )
+}
+
+export default InitialPage;
