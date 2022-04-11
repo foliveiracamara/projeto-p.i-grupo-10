9747edf33cfbd82d96476ea16ee5daeba4b293dc
julianaesteves <juliana.trindade@bandtec.com.br>
2022-04-10 21:39:05 -0300

    Criando formulários e ajustes

--- projeto-react/onhome/src/App.js
+++ projeto-react/onhome/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import Configuracoes from './pages/Dashboard/Configuracoes/Index';
+import Usuarios from './pages/Dashboard/Usuarios/Index';
 import InitialPage from './pages/InitialPage/InitialPage';
 
 function App() {
   return (
     <div className="App">
-      <InitialPage />
     </div>
   );
 }
