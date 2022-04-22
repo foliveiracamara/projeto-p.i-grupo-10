cfcef85f089115925329f0fb9eb781978b8ec5c3
matheusferreira079 <matheus.matos@bandtec.com.br>
2022-04-22 09:31:49 -0300

    atualizando campos da modelagem DER e adicionando scripts do BD mysql e sql Server

---
+++ Banco-de-Dados/Script_Azure.sql
@@ -0,0 +1,80 @@
+CREATE TABLE Licenca (
+idLicenca INT PRIMARY KEY IDENTITY,
+tipo VARCHAR(45),
+quantComputadores INT,
+dataAquisicao DATETIME,
+valor FLOAT);
+
+CREATE TABLE Empresa (
+idEmpresa INT PRIMARY KEY IDENTITY, 
+nomeFantasia VARCHAR(100),
+cnpj CHAR(14),
+fkLicenca INT);
+
+CREATE TABLE Endereco (
+idEndereco INT PRIMARY KEY IDENTITY,
+cep CHAR(8),
+logadouro VARCHAR(100),
+numero INT,
+bairro VARCHAR(45),
+complemento VARCHAR(30),
+estado VARCHAR(100),
+cidade VARCHAR(100),
+fkEmpresa INT);
+
+CREATE TABLE Contatos (
+idContatos INT PRIMARY KEY IDENTITY,
+telefoneEmpresa VARCHAR(11),
+emailEmpresa VARCHAR(45),
+fkEmpresa INT);
+
+CREATE TABLE Usuario (
+idUsuario INT PRIMARY KEY IDENTITY,
+nomeUsuario VARCHAR(50),
+emailUser VARCHAR(50),
+senhaUser VARCHAR(50),
+fkEmpresa INT,
+fkPermissao INT);
+
+CREATE TABLE Permissao (
+idPermissao INT PRIMARY KEY IDENTITY,
+admin CHAR(1),
+fkUsuario INT);
+
+CREATE TABLE Periodo (
+idPeriodo INT PRIMARY KEY IDENTITY,
+mensal CHAR(1),
+semestral CHAR(1),
+anual CHAR(1));
+
+CREATE TABLE Computadores (
+idComputador INT PRIMARY KEY IDENTITY,
+sistemaOperacional VARCHAR(20),
+modeloProcessador VARCHAR(50),
+idProcessador VARCHAR(30),
+tamanhoDisco FLOAT,
+tamanhoDiscoSecundario FLOAT,
+tamanhoRam FLOAT,
+fkUsuario INT);
+
+CREATE TABLE Monitoramento (
+idMonitoramento INT PRIMARY KEY IDENTITY,
+processadorLogico INT,
+processadorFisico INT,
+usandoCpu FLOAT,
+usandoDisco FLOAT,
+usandoRam FLOAT,
+dataHoraCaptura VARCHAR(19),
+tempoLigada VARCHAR(15),
+fkComputador INT);
+
+CREATE TABLE Processo (
+idProcesso INT PRIMARY KEY IDENTITY,
+nomeProcesso VARCHAR(100),
+usoCpu FLOAT,
+usoMemoria FLOAT,
+usoGpu FLOAT,
+dataCaptura VARCHAR(19),
+fkComputador INT);
+
+INSERT INTO Usuario(nomeUsuario, emailUser, senhaUser) VALUES ('admimOnHome', 'admin@onhome.com', 'admin@admin');
---
+++ Banco-de-Dados/Script_Mysql.sql
@@ -0,0 +1,84 @@
+CREATE DATABASE onhome;
+
+USE onhome;
+
+CREATE TABLE IF NOT EXISTS Licenca (
+idLicenca INT PRIMARY KEY AUTO_INCREMENT,
+tipo VARCHAR(45),
+quantComputadores INT,
+dataAquisicao DATETIME,
+valor DOUBLE);
+
+CREATE TABLE IF NOT EXISTS Empresa (
+idEmpresa INT PRIMARY KEY AUTO_INCREMENT, 
+nomeFantasia VARCHAR(100),
+cnpj CHAR(14),
+fkLicenca INT);
+
+CREATE TABLE IF NOT EXISTS Endereco (
+idEndereco INT PRIMARY KEY AUTO_INCREMENT,
+cep CHAR(8),
+logadouro VARCHAR(100),
+numero INT,
+bairro VARCHAR(45),
+complemento VARCHAR(30),
+estado VARCHAR(100),
+cidade VARCHAR(100),
+fkEmpresa INT);
+
+CREATE TABLE IF NOT EXISTS Contatos (
+idContatos INT PRIMARY KEY AUTO_INCREMENT,
+telefoneEmpresa VARCHAR(11),
+emailEmpresa VARCHAR(45),
+fkEmpresa INT);
+
+CREATE TABLE IF NOT EXISTS Usuario (
+idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+nomeUsuario VARCHAR(50),
+emailUser VARCHAR(50),
+senhaUser VARCHAR(50),
+fkEmpresa INT,
+fkPermissao INT);
+
+CREATE TABLE IF NOT EXISTS Permissao (
+idPermissao INT PRIMARY KEY AUTO_INCREMENT,
+admin CHAR(1),
+fkUsuario INT);
+
+CREATE TABLE IF NOT EXISTS Periodo (
+idPeriodo INT PRIMARY KEY AUTO_INCREMENT,
+mensal CHAR(1),
+semestral CHAR(1),
+anual CHAR(1));
+
+CREATE TABLE IF NOT EXISTS Computadores (
+idComputador INT PRIMARY KEY AUTO_INCREMENT,
+sistemaOperacional VARCHAR(20),
+modeloProcessador VARCHAR(50),
+idProcessador VARCHAR(30),
+tamanhoDisco DOUBLE,
+tamanhoDiscoSecundario DOUBLE,
+tamanhoRam DOUBLE,
+fkUsuario INT);
+
+CREATE TABLE IF NOT EXISTS Monitoramento (
+idMonitoramento INT PRIMARY KEY AUTO_INCREMENT,
+processadorLogico INT,
+processadorFisico INT,
+usandoCpu DOUBLE,
+usandoDisco DOUBLE,
+usandoRam DOUBLE,
+dataHoraCaptura VARCHAR(19),
+tempoLigada VARCHAR(15),
+fkComputador INT);
+
+CREATE TABLE IF NOT EXISTS Processo (
+idProcesso INT PRIMARY KEY AUTO_INCREMENT,
+nomeProcesso VARCHAR(100),
+usoCpu DOUBLE,
+usoMemoria DOUBLE,
+usoGpu DOUBLE,
+dataCaptura VARCHAR(19),
+fkComputador INT);
+
+INSERT INTO Usuario(nomeUsuario, emailUser, senhaUser) VALUES ('admimOnHome', 'admin@onhome.com', 'admin@admin');
